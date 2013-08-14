4732dae87f74e9be46ecdba752fc233b1d83eca7
Marko Mikulicic <mmikulicic@gmail.com>
2013-08-14 19:47:33 +0100

    Give a shape to types and visitors

--- cmd/glc.go
+++ cmd/glc.go
@@ -9,45 +9,89 @@ import (
 	"log"
 )
 
-type visitor struct {
-	depth int
+const (
+	Int8 PrimitiveType = iota
+	Int16
+	Int32
+	Int64
+	Uint8
+	Uint16
+	Uint32
+	Uint64
+	String
+)
+
+type Symbol interface {
 }
 
-func (v *visitor) Indent() {
-	for i := 0; i < v.depth; i++ {
-		fmt.Printf(" ")
-	}
+// types
+
+type Type interface {
 }
 
-func (v *visitor) Visit(node ast.Node) ast.Visitor {
-	next := &visitor{v.depth + 1}
+type PrimitiveType uint
 
+type MapType struct {
+	Key   Type
+	Value Type
+}
+
+type SliceType struct {
+	Value Type
+}
+
+//
+type SymbolMap map[string]Symbol
+
+// values
+
+type Function struct {
+}
+
+type Value struct {
+	typ Type
+}
+
+// visitors
+
+type Scope struct {
+	Symbols SymbolMap
+	Parent  *Scope
+}
+
+func NewScope(parent *Scope) *Scope {
+	return &Scope{map[string]Symbol{}, parent}
+}
+
+type ModuleVisitor struct {
+	*Scope
+}
+
+// contains common state shared accross the function
+type FunctionVisitor struct {
+}
+
+// contains scope local to a block
+type BlockVisitor struct {
+	*Scope
+	*FunctionVisitor
+}
+
+func (v *ModuleVisitor) Visit(node ast.Node) ast.Visitor {
 	if node != nil {
-		v.Indent()
-
 		switch n := node.(type) {
 		case *ast.FuncDecl:
 			fmt.Printf("FUNC DECL %s: %#v\n", n.Name, n)
 			if n.Body != nil {
-				ast.Walk(next, n.Body)
+				fv := &FunctionVisitor{}
+				ast.Walk(&BlockVisitor{NewScope(v.Scope), fv}, n.Body)
 			}
 			return nil
-		case *ast.ExprStmt:
-			v.Indent()
-			fmt.Printf("EXPR STATEMENT %#v\n", n)
 		case *ast.DeclStmt:
-			v.Indent()
 			fmt.Printf("DECL STMT %#v\n", n)
-		case *ast.AssignStmt:
-			v.Indent()
-			if n.Tok == token.DEFINE {
-				fmt.Printf("DEFINE ASSIGN STMT %#v\n", n)
-			} else {
-				fmt.Printf("PLAIN ASSIGN STMT %#v ... %#v\n", n, n.Lhs[0])
-			}
 		default:
-			fmt.Printf("-------- %#v\n", node)
-			return next
+			fmt.Printf("----- Module visitor: UNKNOWN %#v\n", node)
+			return v
 		}
 	} else {
 		//		fmt.Printf("popping\n")
@@ -55,9 +99,33 @@ func (v *visitor) Visit(node ast.Node) ast.Visitor {
 	return nil
 }
 
+func (v *BlockVisitor) Visit(node ast.Node) ast.Visitor {
+	if node != nil {
+		switch n := node.(type) {
+		case *ast.ExprStmt:
+			fmt.Printf("EXPR STATEMENT %#v\n", n)
+		case *ast.DeclStmt:
+			fmt.Printf("DECL STMT %#v\n", n)
+		case *ast.AssignStmt:
+			if n.Tok == token.DEFINE {
+				fmt.Printf("DEFINE ASSIGN STMT %#v\n", n)
+			} else {
+				fmt.Printf("PLAIN ASSIGN STMT %#v ... %#v\n", n, n.Lhs[0])
+			}
+		default:
+			fmt.Printf("----- Function visitor: UNKNOWN %#v\n", node)
+			return v
+		}
+	} else {
+		//		fmt.Printf("popping\n")
+	}
+	return nil
+
+}
+
 func CompileFile(tree *ast.File) error {
 	fmt.Printf("compiled %#v\n", tree)
-	v := &visitor{}
+	v := &ModuleVisitor{NewScope(nil)}
 	ast.Walk(v, tree)
 
 	DumpToFile(tree, "/tmp/ast")
@@ -66,7 +134,7 @@ func CompileFile(tree *ast.File) error {
 
 func OpenAndCompileFile(name string) error {
 	var fset token.FileSet
-	ast, err := parser.ParseFile(&fset, name, nil, 0)
+	ast, err := parser.ParseFile(&fset, name, nil, parser.ParseComments)
 	if err != nil {
 		return err
 	}
--- testdata/test1.go
+++ testdata/test1.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 )
 
+// main func
 func main() {
 	var x int = 1 + 2
 	y := x * 13
 	y = y + 1
+
 	y(0)[1] = y + 2
 	fmt.Println(x, y)
 }
